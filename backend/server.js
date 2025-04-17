332783fc6606ae1e98526e1c3fb79edaac12bd5a
Mfundo Moloya <moloyamfundo.com>
2025-04-17 22:26:58 +0200

    set up database connection

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
@@ -18,9 +19,10 @@ app.use('/api/users', userRoutes);
 app.use('/api/services', serviceRoutes);
 
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.log(err));
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error(err));
+
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Smart Campus Services Portal API');
