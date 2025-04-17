f7f39d01d47af30147adf9b8743ff344cde6d49c
Mfundo Moloya <moloyamfundo@gmail.com>
2025-04-17 14:09:22 +0200

    setting up tailwind css plugin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold">Smart Campus Services Portal</h1>
+      <button
+        onClick={() => setCount(count + 1)}
+        className="mt-4 bg-blue-500 text-white rounded-lg py-2 px-4 hover:bg-blue-600"
+      >
+        Count is {count}
+      </button>
+    </div>
+  );
 }
 
-export default App
+export default App;
