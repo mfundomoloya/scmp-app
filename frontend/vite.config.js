848155d7b0af478be44cef02d9e085f9af55a72d
Mfundo Moloya <moloyamfundo.com>
2025-04-19 20:49:32 +0200

    fixing tailwind display

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import postcss from '@tailwindcss/postcss';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    css: {
+        postcss: {
+            plugins: [
+                postcss(), // Use the new PostCSS plugin
+            ],
+        },
+    },
+});
