e86e5602f89bcd0c8d1807be24a3cca4e1ede412
Mfundo Moloya <moloyamfundo.com>
2025-04-20 00:21:10 +0200

    adding logs checking for token verification for better error logs.

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -5,10 +5,13 @@ module.exports = async (req, res, next) => {
   if (!token) return res.status(401).json({ msg: 'No token, authorization denied' });
 
   try {
+    console.log('Verifying token');
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded.user;
+    console.log('Token verified:', { user: req.user });
     next();
   } catch (err) {
+    console.error('Token verification error:', err);
     res.status(401).json({ msg: 'Token is not valid' });
   }
 };
